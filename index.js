d9e4e542219d92fa79d53a3428c99b8db853787d
Ubuntu <ubuntu@ip-172-31-21-26.ap-northeast-1.compute.internal>
2015-12-14 07:49:32 +0000

    comments added

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ var app=express();
 var https= require("https");
 
 
-//making all files static
+//open at http://52.68.7.30/
+
+//making all files in directory ./static publically accessable
 app.use(express.static(__dirname + '/static'));
 
 //api path
 app.get("/issues",function(req,resp,next){
 
-//resp.send("dd")
+//getting repo url from querystring
 	var url= req.query["url"] || "";
+	//regex to verify if its a repo url
 	var mat=url.match(/https?:\/\/([^\.]+\.)?github\.com\/([^\/]+\/[^\/]+)/)
+	//if doesnot match, send error
 	if(!mat) return resp.status(403).send("not a repo url");
 
+//api end point
 	api_ = "/search/issues?q=repo:"+mat[2]+'+is:open+is:issue'
 	time_24hrsback=(new Date((new Date()).getTime()-24*60*60*1000)).toISOString();
   	time_7daysback=(new Date((new Date()).getTime()-7*24*60*60*1000)).toISOString();
 
-
+//preparing output data
   	outp={ t24h:-1, t24h_7d: -1, t7d:-1};
 
    
@@ -31,7 +36,6 @@ app.get("/issues",function(req,resp,next){
 		  res.on('end', function() {
 
 		  	outp.t24h=JSON.parse(body).total_count;
-		  	//console.log("24")
 		  	send(resp);
 		  });
 	})
@@ -50,7 +54,6 @@ app.get("/issues",function(req,resp,next){
 		  res.on('end', function() {
 
 		  	outp.t24h_7d=JSON.parse(body).total_count;
-		  	//console.log("24-7")
 		  	send(resp);
 		  });
 	})
@@ -70,7 +73,6 @@ app.get("/issues",function(req,resp,next){
 		  res.on('end', function() {
 
 		  	outp.t7d=JSON.parse(body).total_count;
-		  	//console.log("7")
 		  	send(resp);
 		  });
 	})
@@ -80,12 +82,13 @@ app.get("/issues",function(req,resp,next){
 	});
 
 
-//check if all 3 data are available, if yes, send response
+//check if all 3 data are available, if yes, send response. otherwise, do nothing
 function send(res){
 if(outp.t7d>-1 && outp.t24h>-1 && outp.t24h_7d>-1 )
 	res.json(outp);
 }
 
+//returns generic get request parameters combined with api endpoint
 function opts(path){
 
 var options = {
@@ -100,4 +103,5 @@ return options
 }
 });
 
+//start app at port 80
 app.listen(80);
